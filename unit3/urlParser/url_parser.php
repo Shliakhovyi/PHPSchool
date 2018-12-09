f3a0f1762414baba5109044f93a9bf0f751e6c4e
Shliakhovyi <shliakhovyi.sviatoslav@pdffiller.team>
2018-12-09 18:21:56 +0200

    add function one function urlParser for all cases

--- unit3/urlParser/url_parser.php
+++ unit3/urlParser/url_parser.php
@@ -2,20 +2,19 @@
 
 $url = getopt('u:', array("url:"));
 
-//if command run php url_parser.php -u <url>
-if (isset($url["u"])) {
-	$host = parse_url($url["u"], PHP_URL_HOST);
+function urlParser($urlstring) {
+	$host = parse_url($urlstring, PHP_URL_HOST);
         $hostparts = explode('.', $host);
         $topleveldomain = implode(".", array_slice($hostparts, 2));
-        $scheme = parse_url($url["u"], PHP_URL_SCHEME);
-        $path = parse_url($url["u"], PHP_URL_PATH);
-        $query = parse_url($url["u"], PHP_URL_QUERY);
+        $scheme = parse_url($urlstring, PHP_URL_SCHEME);
+        $path = parse_url($urlstring, PHP_URL_PATH);
+        $query = parse_url($urlstring, PHP_URL_QUERY);
 
-	//query string splitter
+        //query string splitter
         parse_str($query, $query_array);
 
         $path_parts = pathinfo($path);
-        $hash = parse_url($url["u"], PHP_URL_FRAGMENT);
+        $hash = parse_url($urlstring, PHP_URL_FRAGMENT);
 
         null !== $scheme ? var_dump("scheme (" . $scheme . ")") : "";
         null !== $host ? var_dump("host (" . $host . ")") : "";
@@ -26,46 +25,19 @@ if (isset($url["u"])) {
         isset($path_parts['extension']) ? var_dump("file suffix/extension (" . $path_parts['extension'] . ")") : "";
         null !== $query ? var_dump("query (" . $query . ")") : "";
         null !== $hash ? var_dump("hash (" . $hash . ")") : "";
+}
+
+//if command run: php url_parser.php -u <url>
+if (isset($url["u"])) {
+	$urlstring = $url["u"];
+	urlParser($urlstring);
 //command run: url_parser.php --url=<url>
 } elseif (isset($url["url"])) {
-	$host = parse_url($url["url"], PHP_URL_HOST);
-        $hostparts = explode('.', $host);
-        $topleveldomain = implode(".", array_slice($hostparts, 2));
-        $scheme = parse_url($url["url"], PHP_URL_SCHEME);
-        $path = parse_url($url["url"], PHP_URL_PATH);
-        $query = parse_url($url["url"], PHP_URL_QUERY);
-        parse_str($query, $query_array);
-        $path_parts = pathinfo($path);
-        $hash = parse_url($url["url"], PHP_URL_FRAGMENT);
-
-        null !== $scheme ? var_dump("scheme (" . $scheme . ")") : "";
-        null !== $host ? var_dump("host (" . $host . ")") : "";
-        null !== $host ? var_dump("subdomain (" . $hostparts[0] . ")") : "";
-        null !== $host ? var_dump("domain (" . $hostparts[1] . ")") : "";
-        null !== $host ? var_dump("top level domain (" . $topleveldomain . ")") : "";
-        null !== $path || $query ? var_dump("resource (" . $path . "?" . $query . ")") : "";
-        isset($path_parts['extension']) ? var_dump("file suffix/extension (" . $path_parts['extension'] . ")") : "";
-        null !== $query ? var_dump("query (" . $query . ")") : "";
-        null !== $hash ? var_dump("hash (" . $hash . ")") : "";
+	$urlstring = $url["url"];
+	urlParser($urlstring);
 //command run: php url_parser.php <url>
 } else {
 	$host = parse_url($argv[1], PHP_URL_HOST);
-	$hostparts = explode('.', $host);
-        $topleveldomain = implode(".", array_slice($hostparts, 2));
-	$scheme = parse_url($argv[1], PHP_URL_SCHEME);
-	$path = parse_url($argv[1], PHP_URL_PATH);
-	$query = parse_url($argv[1], PHP_URL_QUERY);
-	parse_str($query, $query_array);
-	$path_parts = pathinfo($path);
-	$hash = parse_url($argv[1], PHP_URL_FRAGMENT);
-
-	null !== $scheme ? var_dump("scheme (" . $scheme . ")") : "";
-	null !== $host ? var_dump("host (" . $host . ")") : "";
-	null !== $host ? var_dump("subdomain (" . $hostparts[0] . ")") : "";
-	null !== $host ? var_dump("domain (" . $hostparts[1] . ")") : "";
-	null !== $host ? var_dump("top level domain (" . $topleveldomain . ")") : "";
-	null !== $path || $query ? var_dump("resource (" . $path . "?" . $query . ")") : "";
-	isset($path_parts['extension']) ? var_dump("file suffix/extension (" . $path_parts['extension'] . ")") : "";
-	null !== $query ? var_dump("query (" . $query . ")") : "";
-	null !== $hash ? var_dump("hash (" . $hash . ")") : "";
+	$urlstring = $argv[1];
+	urlParser($urlstring);
 }
